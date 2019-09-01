476c752a53cb36d3e7e87008e2123e4bb100270d
EdwardT94 <edwardmutisyathomas@gmail.com>
2019-09-01 17:34:02 +0300

    Just created contact us page

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".for").click(function(){
+    $(".form").show(function);
+});
+$(document).ready(function(){
+    $(".for").click(function(){
+    $(".form").hide(function);
+});
