1dae31ede30f04ff7a3c7e4150a9e6f60be0fe99
EdwardT94 <edwardmutisyathomas@gmail.com>
2019-09-01 22:33:43 +0300

    created about us page

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".for").click(function(){
-    $(".form").show(function);
-});
-$(document).ready(function(){
-    $(".for").click(function(){
-    $(".form").hide(function);
-});
